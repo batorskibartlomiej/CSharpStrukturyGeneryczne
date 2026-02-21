fb2c7364e0426313164be8e9adb4cda4271bf820
Bartek <batorskib@gmail.com>
2026-02-21 12:09:32 +0100

    2_4 Kolejka

---
+++ 2_KolekcjaGeneryczne/Pracownik.cs
@@ -0,0 +1,15 @@
+﻿namespace _2_KolekcjaGeneryczne
+{
+    public class Pracownik
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        //public Pracownik(string Imie, string Nazwisko)
+        //{
+        //    this.Imie = Imie;
+        //    this.Nazwisko = Nazwisko;
+        //}
+
+    }
+}
--- 2_KolekcjaGeneryczne/Program.cs
+++ 2_KolekcjaGeneryczne/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace _2_KolekcjaGeneryczne
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Pracownik> pracownicy = new List<Pracownik>
+            {
+                new Pracownik { Imie = "Marcin", Nazwisko = "Nowak"},
+                new Pracownik { Imie = "Jan", Nazwisko = "Nowak"},
+                new Pracownik { Imie = "Tomasz", Nazwisko = "Nowak"},
+             };
+
+            pracownicy.Add(new Pracownik { Imie = "Ania", Nazwisko = "Kos" });
+
+            foreach (var pracownik in pracownicy)
+            {
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+            for (int i = 0; i < pracownicy.Count; i++)
+            {
+                Console.WriteLine(pracownicy[i].Imie +" "+ pracownicy[i].Nazwisko);
+            }
+
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Jan", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Bartek", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Nowak" });
+
+
+            while(kolejka.Count > 0)
+            {
+                var pracownik = kolejka.Dequeue();
+                Console.WriteLine("usuwamy z kolejki " +pracownik.Imie+" "+pracownik.Nazwisko);
+            }
+
         }
     }
 }
